4da3be5cfc1f92ce175357b97852f9a20b0093e9
Lowell Bander <lowellbander@gmail.com>
2015-12-20 19:20:05 -0800

    implemented predict, doesn't pass submission for some reason

--- ex2/predict.m
+++ ex2/predict.m
@@ -15,11 +15,15 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+p = X * theta;
 
-
-
-
-
+for i = 1:m,
+  if (p(i) <= .5)
+    p(i) = 0;
+  else
+    p(i) = 1;
+  end
+end
 
 % =========================================================================
 
